4154e55d04793be9691b504773f01cef6c222821
Allan-BLS <145233620+Allan-BLS@users.noreply.github.com>
2025-09-21 16:36:29 -0300

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renderiza campos do Login na página inicial', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const email =
+    screen.queryByLabelText(/e-mail/i) ||
+    screen.queryByPlaceholderText(/email/i) ||
+    screen.queryByTestId('email');
+
+  const senha =
+    screen.queryByLabelText(/senha/i) ||
+    screen.queryByPlaceholderText(/senha/i) ||
+    screen.queryByTestId('senha');
+
+  expect(email).toBeInTheDocument();
+  expect(senha).toBeInTheDocument();
 });
